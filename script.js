b48db45caf20c6b7f11fc0811f5a99dcc91f1f74
sebastiandoe5 <39373619+sebastiandoe5@users.noreply.github.com>
2018-12-28 20:33:05 +0000

    Add 'click to toggle fullscreen'

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ $(function() {
 
     $("main").css("height", "calc(100vh - " + $(".infoBar").height() + "px)");
 });
+
+function toggleFullScreen() {
+    if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+     (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+      if (document.documentElement.requestFullScreen) {  
+        document.documentElement.requestFullScreen();  
+      } else if (document.documentElement.mozRequestFullScreen) {  
+        document.documentElement.mozRequestFullScreen();  
+      } else if (document.documentElement.webkitRequestFullScreen) {  
+        document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+      }  
+    } else {  
+      if (document.cancelFullScreen) {  
+        document.cancelFullScreen();  
+      } else if (document.mozCancelFullScreen) {  
+        document.mozCancelFullScreen();  
+      } else if (document.webkitCancelFullScreen) {  
+        document.webkitCancelFullScreen();  
+      }  
+    }  
+  }
